f8ae24a7452928222f632d6c1c1f8d3b3c520245
TheLittleBro122 <mynameiscutepanda@gmail.com>
2021-01-05 07:48:28 +0800

    Changed console displays

--- src/app.py
+++ src/app.py
@@ -36,66 +36,66 @@ def screen_mouse():
 
 # Main Sequence
 if __name__ == "__main__":
-    print("Starting Virus in 2 seconds")
+    print("-----VIRUS START IN 2 SECONDS-----")
     time.sleep(2)
     
     # Rotates the screen while mouse clicks
-    print("Screen and Mouse")
+    print("[CONTROL] Rotating Screen, Spam Mouse")
     screen_mouse()
 
-    print("Waiting 5 seconds")
+    print("[TIMER] 5 seconds")
     time.sleep(5)
 
     # Sends 1 threat message
-    print("Sending threat messages")
+    print("[ENLIGHTEN] Enlightening target")
     threat_messages(2)
 
-    # Rotates the screen while mouse clicks
-    print("Screen and Mouse 2x")
+    # Rotates the screen while mouse clicks 2x
+    print("[CONTROL] Rotating Screen, Spam Mouse")
     screen_mouse()
     screen_mouse()
 
-    print("Waiting 5 seconds")
+    print("[TIMER] 5 seconds")
     time.sleep(5)
 
     # Rotates the screen to the right
-    print("Screen tilted to right")
+    print("[CONTROL] Rotate screen to the right")
     screenrotator.change_display_direction(270)
     
     # Literally destroy the keyboard
-    print("Destroying keyboard")
+    print("[CONTROL] Spam keys")
     mousekeyboard.destroy_keyboard()
 
     for i in range(10):
-        print("Alt f4")
+        print("[KEYBOARD] Alt F4")
         mousekeyboard.alt_f4()
         time.sleep(2)
 
-    print("Screen and Mouse")
+    print("[CONTROL] Rotating Screen, Spam Mouse")
     screen_mouse()
 
+    print("[TIMER] 10 seconds")
     time.sleep(10)
 
     # Rotates the screen to the right
-    print("Screen tilted to right")
+    print("[CONTROL] Rotate screen to the right")
     screenrotator.change_display_direction(270)
 
-    # Keeps the screen right for 1 minute
-    print("Waiting 30 seconds")
+    # Keeps the screen right for 1 minute (Just to make the victim think that it's forever stuck)
     time.sleep(30)
 
     # Rotates the screen back to default
-    print("Screen back to default")
+    print("[CONTROL] Rotates screen back to default")
     screenrotator.change_display_direction(0)
 
     # Literally destroy the keyboard
-    print("Destroying keyboard")
+    print("[CONTROL] Spam keys")
     mousekeyboard.destroy_keyboard()
 
     time.sleep(5)
 
-    print("Showing troll message")
+    print("[TEEHEE] Show the the user got TROLLED LMFAOOO")
     mousekeyboard.end()
 
-    print("VIRUS DONE!")
+    print("-----\"VIRUS\" IS DONE! THANKS FOR WATCHING-----")
 
